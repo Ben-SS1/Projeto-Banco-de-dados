a5ae718015f2e02fdf7c9297f581474d2a67d109
Eleazar Benjamim <100734437+Ben-SS1@users.noreply.github.com>
2024-07-23 17:16:56 -0300

    Tabelas criadas e povoamento e select adicionados

---
+++ povoamento.sql
@@ -0,0 +1,25 @@
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
+    ("12345678912", "Xerox Rolmes", "Não-binário", "1846-03-11", "xerox@email.com", "Rua do Padeiro", "221b", "Oestemosteiro", "Londres", "BA");
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email) VALUES 
+    ("73554931257", "Leonardo Freitas", "Masculino", "1999-06-25", "leofreitas@gmail.com");
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, cidade, UF) VALUES 
+    ("4556254210", "Alberto Alcantara", "Masculino", "2004-11-31", "XxX_betinhogamer_XxX_minecraft_XxX@gmail.com", "Alameda das Araucárias", "123", "Campinas", "SP");
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
+    ("12345678912", "Deide Costa", "Feminino", "1846-03-11", "xerox@email.com", "Rua do Padeiro", "221b", "Oestemosteiro", "Londres", "BA");
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
+    ("12345678912", "Giuseppe Camolli", "Masculino", "1979-06-16", "pikapika@pokemon.com", "Rua do Padeiro", "221b", "Oestemosteiro", "Londres", "BA");
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, UF) VALUES 
+    ("10192834556", "Joana Joaquina", "Feminino", "1999-01-01", "jojo@gmail.com", "FL");
+
+
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
+    ("99999999999", 
+    "寿限無 寿限無 五劫の擦り切れ 海砂利水魚の 水行末 雲来末 風来末 食う寝る処に住む処 やぶら柑子のぶら柑子 パイポパイポ パイポのシューリンガン 
+    シューリンガンのグーリンダイ グーリンダイのポンポコピーのポンポコナーの 長久命の長助", 
+    "Agênero", "2002-03-11", "ponpokopinoponpokona@yahoo.com", "Rua Chōkyūmei-no Chōsuke", "220", "Afogados", "Recife", "PE");
+
+INSERT INTO funcionario (cpf, salario, d_contratacao) VALUES
+    ("12345678912", 30000,"2009-02-01");
+INSERT INTO funcionario (cpf, salario, d_contratacao) VALUES
+    ("73554931257", 2000,"2024-02-01");
+
---
+++ select.sql
@@ -0,0 +1,17 @@
+SELECT cpf, nome, genero, d_nasc FROM pessoa;
+SELECT * FROM funcionario;
+SELECT funcionario.salario, pessoa.nome FROM funcionario, pessoa WHERE salario > 1000 AND pessoa.cpf=funcionario.cpf;
+SELECT DISTINCT d_contratacao FROM funcionario;
+SELECT P.d_nasc AS 'Data de nascimento' FROM pessoa AS P
+
+(SELECT P.nome, P.d_nasc, P.rua FROM pessoa AS P WHERE P.nome LIKE "%Rolmes%") UNION (
+    SELECT P.nome, P.d_nasc, P.rua FROM pessoa AS P WHERE P.nome LIKE "%Alcantara%")
+
+(SELECT P.nome, EP.cpf FROM pessoa AS P, est_pos AS EP WHERE P.cpf=EP.cpf
+) INTERSECT (
+    SELECT P.nome, PS.cpf FROM pessoa AS P, prof_sub AS PS WHERE P.cpf=PS.cpf)
+
+(SELECT P.nome, F.cpf FROM pessoa AS P, funcionario AS F WHERE P.cpf=F.cpf
+) EXCEPT (
+    SELECT P.nome, Prof.cpf FROM pessoa AS P, professor AS Prof WHERE P.cpf=Prof.cpf)
+
