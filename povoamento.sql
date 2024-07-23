51cec56516d535b0f93c96d1812fffc51b567920
Charlys Farias <charlesaugustoararaazul@gmail.com>
2024-07-23 19:53:46 -0300

    adicionando registros na tabela pessoas

--- povoamento.sql
+++ povoamento.sql
@@ -1,22 +1,46 @@
+--- Povoamento da Tabela Pessoas
+--- 18 registros na Tabela Pessoas
+
+
 INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
-    ("12345678912", "Xerox Rolmes", "Não-binário", "1846-03-11", "xerox@email.com", "Rua do Padeiro", "221b", "Oestemosteiro", "Londres", "BA");
+    ("", "", "", "", "", "", "", "", "", "");
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
+    ("12345678912", "Xerox Rolmes", "Neutrois", "1846-03-11", "xerox@email.com", "Rua do Padeiro", "221b", "Oestemosteiro", "Londres", "BA");
 INSERT INTO pessoa (cpf, nome, genero, d_nasc, email) VALUES 
     ("73554931257", "Leonardo Freitas", "Masculino", "1999-06-25", "leofreitas@gmail.com");
 INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, cidade, UF) VALUES 
     ("4556254210", "Alberto Alcantara", "Masculino", "2004-11-31", "XxX_betinhogamer_XxX_minecraft_XxX@gmail.com", "Alameda das Araucárias", "123", "Campinas", "SP");
-INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
-    ("12345678912", "Deide Costa", "Feminino", "1846-03-11", "xerox@email.com", "Rua do Padeiro", "221b", "Oestemosteiro", "Londres", "BA");
-INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
-    ("12345678912", "Giuseppe Camolli", "Masculino", "1979-06-16", "pikapika@pokemon.com", "Rua do Padeiro", "221b", "Oestemosteiro", "Londres", "BA");
+INSERT INTO pessoa (cpf, nome, genero) VALUES 
+    ("12345678912", "Deide Costa", "Feminino");
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email) VALUES 
+    ("12345678912", "Giuseppe Camolli", "Masculino", "1979-06-16", "pikapika@pokemon.com");
 INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, UF) VALUES 
     ("10192834556", "Joana Joaquina", "Feminino", "1999-01-01", "jojo@gmail.com", "FL");
-
-
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
+    ("", "Tonari no Totoro", "Neutrois", "1988-04-16", "meu.amigo.totoro@studio.ghibli.com", "Rua  Koganei", "00", "Toquio", "Ja");
+INSERT INTO pessoa (cpf, nome, genero, email, bairro, cidade, UF) VALUES 
+    ("", "Bob Gray", "Agênero", "pennywise.lives@stephen.king.com","Esgotos", "Derry", "MA");
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, cidade, UF) VALUES 
+    ("", "Alice Oseman", "Feminino", "1994-10-16", "alice.oseman@gmail.com", "Chatham", "UK");
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
+    ();
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
+    ("", "", "", "", "", "", "", "", "", "");
 INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
     ("99999999999", 
     "寿限無 寿限無 五劫の擦り切れ 海砂利水魚の 水行末 雲来末 風来末 食う寝る処に住む処 やぶら柑子のぶら柑子 パイポパイポ パイポのシューリンガン 
     シューリンガンのグーリンダイ グーリンダイのポンポコピーのポンポコナーの 長久命の長助", 
     "Agênero", "2002-03-11", "ponpokopinoponpokona@yahoo.com", "Rua Chōkyūmei-no Chōsuke", "220", "Afogados", "Recife", "PE");
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
+    ("", "", "", "", "", "", "", "", "", "");
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
+    ("", "", "", "", "", "", "", "", "", "");
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
+    ("", "", "", "", "", "", "", "", "", "");
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
+    ("", "", "", "", "", "", "", "", "", "");
+INSERT INTO pessoa (cpf, nome, genero, d_nasc, email, rua, numero, bairro, cidade, UF) VALUES 
+    ("", "", "", "", "", "", "", "", "", "");
 
 INSERT INTO funcionario (cpf, salario, d_contratacao) VALUES
     ("12345678912", 30000,"2009-02-01");
